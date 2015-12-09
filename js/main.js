69715f155edd511676b7568ff81a5680bd024911
Proro4 <proro4@gmail.com>
2015-12-09 20:43:05 +0200

    v0.1.1

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ $('.lang .lang-but').on('click',function(){
 	})
 	function chat(){
 		$('.chat-mailing').append('<div class="chat-interlocutor left"><div class="chat-img left"><img src="img/photo/chat-img1.jpg" alt=""></div><div class="chat-text left arrow-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </div><div class="chat-time left">14:33</div></div>')
-	}setTimeout(chat,3000);
+	}setTimeout(chat,5000);
 	$('.chat-message a').on('click',function(){
 		var textMessage = $('.chat-message textarea').val();
 		$('.chat-mailing').append('<div class="chat-user right"><div class="chat-img right"><img src="img/photo/chat-img1.jpg" alt=""></div><div class="chat-text right arrow-right">'+textMessage+'</div><div class="chat-time left">14:33</div></div>')
